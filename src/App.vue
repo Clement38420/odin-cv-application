260fd2ea60d69084058d2ca5853024e3625c2109
Clement Charbonnel <c.charbo38@gmail.com>
2025-09-13 12:03:39 +0200

    feat: add editable field base component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseEditableField from '@/components/BaseEditableField.vue'
+</script>
 
 <template>
   <main class="cv">
-    <div class="personal-info"></div>
+    <div class="personal-info">
+      <BaseEditableField></BaseEditableField>
+    </div>
     <div class="experiences"></div>
   </main>
 </template>
@@ -12,8 +16,11 @@
   --bg: hsl(0, 0%, 95%);
   --bg-light: hsl(0, 0%, 100%);
   --bg-dark: hsl(0, 0%, 90%);
+
   --text: hsl(0, 0%, 5%);
   --text-muted: hsl(0, 0%, 40%);
+  --text-light: hsl(0, 0%, 95%);
+
   --card-shadow: 0 2px 2px hsla(0, 0%, 0%, 0.15), 0 4px 4px hsla(0, 0%, 0%, 0.05);
 
   --cv-background: white;
@@ -44,5 +51,10 @@ body {
 
 .personal-info {
   background-color: var(--cv-primary);
+  padding: 16px;
+}
+
+.personal-info * {
+  color: var(--text-light);
 }
 </style>
---
+++ src/components/BaseEditableField.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="field">
+    <p class="content">Clément Charbonnel</p>
+    <div class="edit-message">
+      <span class="material-icons edit-icon">edit</span>
+      Click to edit
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.field {
+  position: relative;
+}
+
+.field:hover {
+  cursor: pointer;
+}
+
+.content {
+  font-size: 2rem;
+}
+
+.edit-message {
+  opacity: 0;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  background-color: hsla(0, 0%, 70%, 0.8);
+  transition: opacity 0.2s;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  translate: -5px 5px;
+  width: calc(100% + 10px);
+}
+
+.field:hover .edit-message {
+  opacity: 1;
+}
+
+.field::after {
+  content: '';
+  position: absolute;
+  display: inline-block;
+  top: -5px;
+  left: -5px;
+  right: -5px;
+  bottom: -5px;
+  border-radius: 8px;
+  box-shadow: 0 0 0 0 transparent;
+  pointer-events: none;
+  transition: box-shadow 0.2s;
+}
+
+.field:hover::after {
+  box-shadow: 0 0 0 1px hsla(0, 0%, 70%, 0.8);
+  border-radius: 8px;
+}
+
+.edit-icon {
+  font-size: 1rem;
+  translate: 0 13%;
+  margin-right: 3px;
+}
+</style>
