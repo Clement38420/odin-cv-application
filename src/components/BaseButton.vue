abb354f1e86529c7e34034cbc766b7c8f6f7ac1a
Clément Charbonnel <c.charbo38@gmail.com>
2025-09-18 23:52:23 +0200

    feat: add BaseButton component with customizable styles
    Buttons are reusable and can use various color or style (outlined).

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+defineProps<{
+  type: 'positive' | 'negative' | 'default'
+  outlined?: boolean
+}>()
+
+const buttonTextColor = {
+  positive: 'white',
+  negative: 'white',
+  default: 'white',
+}
+</script>
+
+<template>
+  <button :class="[type, { outlined }]" :style="!outlined ? { color: buttonTextColor[type] } : ''">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button.outlined {
+  background-color: white;
+  color: var(--button-color);
+}
+
+button.negative {
+  --button-color: var(--negative);
+}
+
+button.positive {
+  --button-color: var(--positive);
+}
+
+button.default {
+  --button-color: var(--text-muted);
+  color: var(--text);
+}
+
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  border-radius: 0.8em;
+  box-shadow: var(--button-shadow);
+  transition:
+    box-shadow 0.1s,
+    transform 0.1s;
+  background-color: var(--button-color);
+  border: 1px solid var(--button-color);
+}
+
+button:hover {
+  transform: translateY(-2px) scale(1.04);
+  box-shadow:
+    0 4px 4px hsla(0, 0%, 0%, 0.15),
+    0 8px 8px hsla(0, 0%, 0%, 0.05),
+    0 0 3px var(--button-color);
+}
+
+button:active {
+  transform: translateY(2px) scale(0.98);
+  box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.25);
+}
+</style>
